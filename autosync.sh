a811fec26dace374bae7182b92b26dbaf1edd8ba
alefaccincani <alo.nessy02@gmail.com>
2026-04-20 13:39:47 +0200

    HashTable

--- autosync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/Desktop/LaTeX/Algoritmi
-fswatch -o main.tex Immagini/ | while read change; do
-  git add main.tex Immagini/
-  git commit -m "Auto-sync $(date '+%Y-%m-%d %H:%M')"
-  git push origin main
-done
-
